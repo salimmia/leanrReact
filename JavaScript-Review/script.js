043e86d6184ac847bc90430dd5f4bb11e3a6c928
selim <salimmia3745@gmail.com>
2023-07-21 14:39:04 +0600

    Optional Chaining (24)

--- JavaScript-Review/script.js
+++ JavaScript-Review/script.js
@@ -145,7 +145,7 @@ function getBook(id) {
 
 const books = getBooks();
 
-const book = getBook(2);
+const book = getBook(3);
 
 // const title = book.title;
 // const author = book.author;
@@ -216,9 +216,17 @@ console.log(book.translations.spanish);
 const spanishTranslation = book.translations.spanish || "Not Translated";
 spanishTranslation;
 
-console.log(book.reviews.librarything.reviewsCount);
-const countWrong = book.reviews.librarything.reviewsCount || "no data";
-console.log(countWrong);
+// console.log(book.reviews.librarything.reviewsCount);
+// const countWrong = book.reviews.librarything.reviewsCount || "no data";
+// console.log(countWrong);
 
-const count = book.reviews.librarything.reviewsCount ?? "no data";
-count;
+// const count = book.reviews.librarything.reviewsCount ?? "no data";
+// count;
+function getTotalReviewCount(book) {
+  const goodreads = book.reviews.goodreads.reviewsCount;
+  const librarything = book.reviews.librarything?.reviewsCount ?? 0;
+  librarything;
+  return goodreads + librarything;
+}
+
+console.log(getTotalReviewCount(book));
