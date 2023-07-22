b66ff4ec24aae681b0fcfbc51cdb8a75141eaac5
selim <salimmia3745@gmail.com>
2023-07-22 14:18:20 +0600

    Asynchronous JavaScript: Async/Await (31)

--- JavaScript-Review/script.js
+++ JavaScript-Review/script.js
@@ -301,9 +301,20 @@ const bookAfterUpdate = booksAfterDelete.map((book) =>
 );
 bookAfterUpdate;
 */
+// const fetch = require("node-fetch");
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
 
-fetch("https://jsonplaceholder.typicode.com/todos")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+// console.log("jonas");
 
-console.log("jonas");
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+
+  return data;
+}
+
+const todos = getTodos();
+console.log(todos);
